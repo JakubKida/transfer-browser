498be653f3ed84810bdbd7ea66179fc1ed2c3116
Jakub Kida <jakubjkida@gmail.com>
2023-10-09 12:38:30 +0200

    Initiate app with CRA

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.module.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Test text</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
